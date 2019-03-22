7526d3c3bafdb91ec779fb2cfded78b99644896f
YLQ <2465266566@qq.com>
2019-03-22 16:16:57 +0800

    登录模块业务层

---
+++ src/main/java/LightningProtection/service/UserService.java
@@ -0,0 +1,8 @@
+package LightningProtection.service;
+
+import LightningProtection.pojo.ta_auth_user;
+
+public interface UserService {
+    int userRegister(ta_auth_user adduser);
+    boolean userLogin(String username,String password);
+}
---
+++ src/main/java/LightningProtection/service/imp/UserServiceImp.java
@@ -0,0 +1,26 @@
+package LightningProtection.service.imp;
+
+import LightningProtection.mapper.UserMapper;
+import LightningProtection.pojo.ta_auth_user;
+import LightningProtection.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImp implements UserService {
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public int userRegister(ta_auth_user adduser){
+        return userMapper.userRegister(adduser);
+    }
+
+    @Override
+    public boolean userLogin(String username,String password){
+        List<ta_auth_user> userList = userMapper.userLogin(username,password);
+        return  userList.size() != 0;
+    }
+}
